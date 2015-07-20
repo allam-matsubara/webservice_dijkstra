a6111b4e5ba45947ee967c5195701c08f5e89419
Allam Marcos Campanini Matsubara <allam.matsubara@gmail.com>
2015-07-20 00:27:38 -0300

    Data structure containing all nodes associated withh all its neighbours and the distance between them created. Next step is to implement Dijkstra's with this structure.

--- app/models/path.rb
+++ app/models/path.rb
@@ -3,24 +3,44 @@ class Path < ActiveRecord::Base
   validates :point_a, :point_b, :distance, :name, presence: true
 
   def self.shortest_path(options_hash = {})
+    graph = Hash.new
     map_name    = options_hash["map_name"]
     origin      = options_hash["origin"]
     destination = options_hash["destination"]
     autonomy    = options_hash["autonomy"]
     consumption = options_hash["consumption"]
+    
+    vertices = get_all_vertices(map_name)
+    vertices.each { |v| graph[v.to_sym] = Array.new(find_neighbours(map_name, v)) }
 
-    routes = where(name: map_name)
-    dijkstra(routes, origin, destination)
+    #dijkstra(map_name, origin, destination)
   end
 
-  private
+  def self.dijkstra(map, origin, distination)
+    edges  = Array.new
+    path   = Array.new
+    weight = Array.new
+  end
 
-    def self.dijkstra(routes, origin, distination)
-      edges = Array.new
-      routes.each do |route|
-        edges << [route.point_a, route.point_b, route.distance]
-      end
+  def self.get_all_vertices(map)
+    vertices = Array.new
 
-      print edges
+    where(name: map).each do |path|
+      vertices.concat([path.point_a, path.point_b])
     end
+
+    vertices.uniq
+  end
+
+  def self.find_neighbours(map, vertex)
+    result = Array.new
+
+    where("name = :map AND (point_a = :v OR point_b = :v)", {map: map, v: vertex}).
+    each do |e|
+      result << { v: (e.point_a == vertex ? e.point_b : e.point_a),
+                    distance: e.distance }
+    end
+
+    result
+  end
 end
